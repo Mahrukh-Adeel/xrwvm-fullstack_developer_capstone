35f785cd9feeb99c79647ccc232d4cd54cd3fb45
Mahrukh Adeel <mahrukhadeel5@gmail.com>
2025-06-14 10:42:17 +0000

    FIxed for CI/CD

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,12 +31,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
-    "https://mahrukhadee1-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
+    (
+        "https://mahrukhadee1-8000.theiadockernext-1-labs-"
+        "prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
+    ),
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://mahrukhadee1-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
+    (
+        "https://mahrukhadee1-8000.theiadockernext-1-labs-"
+        "prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
+    )
 ]
-
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [],
 }
@@ -99,16 +104,28 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation."
+            "UserAttributeSimilarityValidator"
+        ),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation."
+            "MinimumLengthValidator"
+        ),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation."
+            "CommonPasswordValidator"
+        ),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation."
+            "NumericPasswordValidator"
+        ),
     },
 ]
 
